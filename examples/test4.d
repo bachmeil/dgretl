01facf414489844312ae77bd48735ff8a7aee694
Lance Bachmeier <lanceb@ksu.edu>
2019-07-26 06:32:41 -0500

    nm

--- examples/test4.d
+++ examples/test4.d
@@ -3,12 +3,11 @@ import std.stdio;
 
 void main() {
 	auto v = TS([1.5, 6.5, 7.5, 12.5, 13.5], [2018,1], 12);
-	gretlexp.dynmodel.print(v);
+	//~ gretlexp.dynmodel.print(v);
 	
 	randInit();
-	auto x = TS(rnorm(60), [1, 1], 1);
-	gretlexp.dynmodel.print(x);
-	//ArmaFit fit = armaGretl(x, 1, 1);
-	armaGretl(x, 1, 1);
-	//~ writeln(fit.coef);
+	auto x = TS(rnorm(600), [1, 1], 1);
+	//~ gretlexp.dynmodel.print(x);
+	ArmaFit fit = armaGretl(x, 0, 1);
+	fit.print();
 }
